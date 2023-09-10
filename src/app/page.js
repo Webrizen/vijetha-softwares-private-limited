f982badaa748b608a380a6d09c2dff4a724ee092
WebRizen <webrizen@gmail.com>
2023-09-10 14:46:37 +0530

    check now

--- src/app/page.js
+++ src/app/page.js
@@ -384,66 +384,139 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="body-font relative">
-        <div className="w-full px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-7xl text-6xl font-medium title-font mb-4 h1">Contact Us</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+      <section className="mb-2 sm:w-4/5 w-full p-3 mx-auto">
+        <div className="flex justify-center">
+          <div className="text-center md:max-w-xl lg:max-w-3xl">
+            <h2 className="mb-12 px-6 text-3xl font-bold">Contact us</h2>
           </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
-                  <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+
+        <div className="flex flex-wrap bg-[rgba(225,225,225,0.1)] py-12 px-5 rounded-2xl">
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
+            <form>
+              <div className="relative mb-4">
+                <label for="Name" className="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="Name" name="Name" className="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+                <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+                <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              </div>
+              <button className="inline-flex text-white bg-gradient-to-r from-red-400 to-amber-500 background-animate border-0 py-2 px-6 focus:outline-none rounded text-base">Send Message</button>
+              <p className="text-xs text-gray-500 mt-3">"Rest assured, your inbox won't be bombarded. By sharing your email with us, you're aiding in safeguarding our website against unwanted traffic, including bots and more."</p>
+            </form>
+          </div>
+          <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
+            <div className="flex flex-wrap">
+              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6 ">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <div className="inline-block rounded-md bg-primary-100 p-4 text-amber-500 bg-amber-200">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" className="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-6 grow">
+                    <p className="mb-2 font-bold dark:text-white">
+                      Technical support
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      support@example.com
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      +1 234-567-89
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
-                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+                <div className="flex items-start">
+                  <div className="shrink-0">
+                    <div className="inline-block rounded-md bg-primary-100 p-4 text-amber-500 bg-amber-200">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" className="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-6 grow">
+                    <p className="mb-2 font-bold dark:text-white">
+                      Sales questions
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      sales@example.com
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      +1 234-567-89
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="p-2 w-full">
-                <div className="relative">
-                  <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
-                  <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+                <div className="align-start flex">
+                  <div className="shrink-0">
+                    <div className="inline-block rounded-md bg-primary-100 p-4 text-amber-500 bg-amber-200">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" className="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-6 grow">
+                    <p className="mb-2 font-bold dark:text-white">Press</p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      press@example.com
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      +1 234-567-89
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="p-2 w-full flex justify-center items-center">
-                <button className="btn">Send Message</button>
+              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+                <div className="align-start flex">
+                  <div className="shrink-0">
+                    <div className="inline-block rounded-md bg-primary-100 p-4 text-amber-500 bg-amber-200">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" className="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-6 grow">
+                    <p className="mb-2 font-bold dark:text-white">Bug report</p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      bugs@example.com
+                    </p>
+                    <p className="text-neutral-500 dark:text-neutral-200">
+                      +1 234-567-89
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <div className='flex flex-row justify-between items-center'>
-                  <a className="text-yellow-500">contact@vijethasoftwares.in</a>
-                  <a className='text-yellow-400'>+91- 8891660573</a></div>
-                <p className="leading-normal my-5">49 Smith St.
-                  <br />Saint Cloud, MN 56301
-                </p>
-                <span className="inline-flex">
-                  <a className="text-gray-500">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div>
+            </div>
+            <div>
+              <iframe
+                width="100%"
+                height="230px"
+                className='rounded-2xl'
+                title="map"
+                src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+                vnb-frameid="627"
+                style={{
+                  filter: 'opacity(0.8)',
+                }}
+              ></iframe>
             </div>
           </div>
         </div>
